c49bc0807309a9ab1c2680be347378e2d98c12eb
Grandpahao <grandpahao@qq.com>
2018-08-15 15:14:37 +0800

    finish actor agent implementation for single actor-critic

--- Simple Actor Critic/agent/actor.py
+++ Simple Actor Critic/agent/actor.py
@@ -8,15 +8,59 @@ class Actor(TFAgent):
         super(Actor, self).__init__(n_ac, lr)
         self.update_target()
 
-    def _net(self, input, trainable):
-        conv1 = tf.contrib.layers.conv2d(
-            input, 32, 8, 4, activation_fn=tf.nn.relu, trainable=trainable)
-        conv2 = tf.contrib.layers.conv2d(
-            conv1, 64, 4, 2, activation_fn=tf.nn.relu, trainable=trainable)
-        conv3 = tf.contrib.layers.conv2d(
-            conv2, 64, 3, 1, activation_fn=tf.nn.relu, trainable=trainable)
+    def _build_net(self):
+        self.input = tf.placeholder(
+            shape=[None, 84, 84, 4], dtype=tf.float32, name='inputs')
+        self.action_select = tf.placeholder(
+            shape=[None], dtype=tf.float32, name='selected_action')
+        self.advantage = tf.placeholder(
+            shape=[None], dtype=tf.float32, name='advantage')
+
+        with tf.variable_scope('actor_accurate'):
+            fc1 = self._net(self.input, trainable=True)
+            self.action = tf.layers.dense(fc1, self.n_ac, trainable=True)
+
+        with tf.variable_scope('actor_target'):
+            fc1_target = self._net(self.input, trainable=False)
+            self.action_target = tf.layers.dense(
+                fc1_target, self.n_ac, trainable=False)
+
+        self.update_target_opr = self._update_target_opr()
+
+        trainable_variables = tf.trainable_variables('actor_accurate')
+        self.loss = self._eval_loss()
+        self.train_opr = self.optimizer.minimize(
+            self.loss,
+            global_step=tf.train.get_global_step(),
+            var_list=trainable_variables
         )
 
-    def _build_net(self):
-        self.input=tf.placeholder(
-            shape = [None, 84, 84, 4], dtype = tf.float32, name = 'inputs')
+    def update(self, ):
+
+    def _eval_loss(self):
+        if self.n_ac > 1:
+            batch_size = tf.shape(self.input)[0]
+            gather_indices = tf.range(batch_size) * \
+                self.n_ac + self.action_select
+            action_prob = tf.gather(tf.reshape(
+                self.action, [-1]), gather_indices)
+            # policy gradient should ascent
+            ad_log_prob = tf.neg(tf.log(action_prob) * self.advantage)
+            return tf.reduce_mean(ad_log_prob)
+        else:
+            raise NotImplementedError
+
+    def _update_target_opr(self):
+        params = tf.trainable_variables('actor_accurate')
+        params = sorted(params, key=lambda v: v.name)
+        target_params = tf.global_variables('actor_target')
+        target_params = sorted(params, key=lambda v: v.name)
+
+        update_opr = []
+        for param, target_param in zip(params, target_params):
+            update_opr.append(target_param.assign(param))
+
+        return update_opr
+
+    def update_target(self):
+        self.sess.run(self.update_target_opr)
--- Simple Actor Critic/agent/tfagent.py
+++ Simple Actor Critic/agent/tfagent.py
@@ -28,23 +28,30 @@ class TFAgent(object):
     def update_target(self, *args, **kwargs):
         raise NotImplementedError
 
-    def _conv_net(self):
-        raise NotImplementedError
-
-    def _lstm_net(self):
-        raise NotImplementedError
+    def _net(self, input, trainable):
+        conv1 = tf.contrib.layers.conv2d(
+            input, 32, 8, 4, activation_fn=tf.nn.relu, trainable=trainable)
+        conv2 = tf.contrib.layers.conv2d(
+            conv1, 64, 4, 2, activation_fn=tf.nn.relu, trainable=trainable)
+        conv3 = tf.contrib.layers.conv2d(
+            conv2, 64, 3, 1, activation_fn=tf.nn.relu, trainable=trainable)
+        )
+        flat1=tf.reshape(conv3, shape = [None, 2048])
+        fc1=tf.layers.dense(
+            flat1, 256, activaion = tf.nn.relu, trainable = trainable)
+        return fc1
 
     def save_model(self, outdir):
-        cur_step = get_global_step()
+        cur_step=get_global_step()
         self.saver.save(
             self.sess,
             os.path.join(outdir, 'model'),
             cur_step,
-            write_meta_graph=False
+            write_meta_graph = False
         )
 
     def load_model(self, outdir):
-        latest_log = tf.train.last_checkpoint(outdir)
+        latest_log=tf.train.last_checkpoint(outdir)
         if latest_log:
             print("Loading model from {}".format(latest_log))
             self.saver.restore(self.sess, latest_log)
